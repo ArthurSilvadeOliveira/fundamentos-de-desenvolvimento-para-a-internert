04a1d99bd846a0c45527f32b9d4a595b858f0d94
ArthurSilvadeOliveira <81304454+ArthurSilvadeOliveira@users.noreply.github.com>
2021-03-24 09:24:38 -0300

    Update index.php

--- quest2php/index.php
+++ quest2php/index.php
@@ -1,5 +1,5 @@
 <h1>Selecione um ano</h1>
 <form action="cod2.php" method="post"> 
    <p>Ano <input type="text" name="cidade" placeholder="cidade" /></p>
- </form> 
-<button>Valide</button>
+   <p><button type="submit">Envia</button></p>
+</form> 
